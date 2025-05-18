fc11c7053f62e479adbff5eaab726c9d025a2fc2
FMM2244 <fatimaalmaaita@gmail.com>
2025-05-18 12:31:17 +0300

    added some explaining comments and created a test domain

--- app.py
+++ app.py
@@ -2,11 +2,42 @@ from flask import Flask, request, jsonify, render_template
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
+from huggingface_hub import whoami
+
+info = whoami()
+print("Connected to Hugging Face as:", info["name"])
+
 app = Flask(__name__)
 
 model_name = "distilgpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# this is a decorator in python, which is a design pattern that allows you to modify the
+# functionality of a function by wrapping it in another function.
+# 
+# basicly when some enter the root of the website it executes the functon below
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# this function executes after the user clicks the ask button
+# it fetches data from the user formats is and create a proper prompt
+# and then sends it to the model
+# after it recives the models respons the
+# the answer is send to be cleaned and then send back to
+# be displayed to the user
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    user_question = data.get('question', '')
+    prompt = create_prompt(user_question, domain_42amman, domain_jordan)
+    inputs = tokenizer(prompt, return_tensors='pt', max_length=105, truncation=True)
+    outputs = model.generate(**inputs, max_length=150, pad_token_id=tokenizer.eos_token_id)
+    answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    clean_answer = answer.replace(prompt, '').strip()
+
+    return jsonify({'answer': clean_answer})
 
 domain_42amman = """
 Q: What is 42Amman?
@@ -108,33 +139,21 @@ Q: What’s the dress code in Jordan?
 A: Jordan is relatively liberal, but modest clothing is appreciated, especially in religious or rural areas.
 """
 
+domain_test = """
+42Amman is a tuition-free coding school in Jordan that uses peer-to-peer, project-based learning. It's part of the global 42 Network.
+It's free for all students 100%.
+"""
+
 def create_prompt(user_question, domain_42amman, domain_jordan):
     prompt = (
         "You are an assistant that answers questions about either \"Jordan\" or \"42Amman.\"\n\n"
-        # f"Here is the user question:\n{user_question}\n\n"
-        # "Use the following information to generate a short, concise, and polite answer:\n\n"
-        # f"Information about 42Amman:\n{domain_42amman}\n\n"
+        f"Here is the user question:\n{user_question}\n\n"
+        "Use the following information to generate a short, concise, and polite answer:\n\n"
+        f"Information about 42Amman:\n{domain_test}\n\n"
         # f"Information about Jordan:\n{domain_jordan}\n\n"
-        # "Please answer politely and keep it brief."
+        "Please answer politely and keep it brief."
     )
     return prompt
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/ask', methods=['POST'])
-def ask():
-    data = request.get_json()
-    user_question = data.get('question', '')
-    prompt = create_prompt(user_question, domain_42amman, domain_jordan)
-
-    inputs = tokenizer(prompt, return_tensors='pt', max_length=1024, truncation=True)
-    outputs = model.generate(**inputs, max_length=150, pad_token_id=tokenizer.eos_token_id)
-    answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-    return jsonify({'answer': answer})
 
 if __name__ == '__main__':
     app.run(debug=True)
